d953c750e1a9eb303dbb3cd822dd29b46a774bc6
Eddie6382 <b07901020@ntu.edu.tw>
2020-02-06 16:04:32 +0800

    hi

--- dfs/graph.py
+++ dfs/graph.py
@@ -29,15 +29,22 @@ GRAY = 1
 BLACK = 2
 
 class Node():
-    def __init__(self):
-        self.discover = 0
-        self.finish = 0
-        self.color = WHITE
+    def __init__(self, idx):
+        self._discover = 0
+        self._finish = 0
+        self._color = WHITE
+        self._idx = idx
+        self._parent = -1
 
     def reset():
-        self.discover = 0
-        self.finish = 0
-        self.color = WHITE
+        self._discover = 0
+        self._finish = 0
+        self._color = WHITE
+        self._parent = -1
+
+    def __str__(self):
+        print("node{}: parent is {}, discover at {}, finish at {}"
+                .format(self._idx, self._parent, self._discover, self._finish))
 
 class Graph():
     def __init__(self, vertices):
@@ -50,26 +57,34 @@ class Graph():
         for i in range(self._v):
             print(i, self._adjList[i])
 
-    def DFStraversal(self, dfslist):
+    def DFStraversal(self, u):
         self._time = self._time + 1
+        u._discover = self._time
+        u._color = GRAY
+        for v in self._adjList[u._idx]:
+            if v._color == WHITE:
+                v._parent = u._idx
+                DFStraversal(self, v)
+        u._color = BLACK
+        self._time = self._time + 1
+        u._finish = self._time
+
         
-
-
 class undirectGraph(Graph):
     def __init__(self, vertices):
         Graph.__init__(self, vertices)
     
-    def addEdge(self, u, v, weight = 1):
-        self._adjList[u].append(v)
-        self._adjMatrix[u][v] = weight
-        self._adjMatrix[v][u] = weight
+    def addEdge(self, m, n, weight = 1):
+        self._adjList[m].append(Node(n))
+        self._adjMatrix[m][n] = weight
+        self._adjMatrix[n][m] = weight
 
 class directGraph(Graph):
     def __init__(self, vertices):
         Graph.__init__(self, vertices)
 
-    def addEdge(self, u, v, weight = 1):
-        self._adjList[u].append(v)
-        self._adjMatrix[u][v] = weight
+    def addEdge(self, m, n, weight = 1):
+        self._adjList[m].append(n)
+        self._adjMatrix[m][n] = weight
     
 
